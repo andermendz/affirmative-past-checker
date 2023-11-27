e791a81a52293607a0e42dd162e00e0a15585b53
Anderson Mendoza <74158361+andermendz@users.noreply.github.com>
2023-11-27 14:45:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,10 @@
-document.getElementById('add').addEventListener('click', function() {
-    const sentences = document.getElementById('sentences');
-    const sentenceDiv = document.createElement('div');
-    sentenceDiv.className = 'sentence flex items-center ';
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'X';
-    removeButton.className = 'remove text-xs px-1 py-0.5 bg-red-500 text-white rounded';
-    removeButton.addEventListener('click', function() {
-        sentences.removeChild(sentenceDiv);
-    });
-    sentenceDiv.appendChild(removeButton);
-
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.className = 'mt-2 ml-2 flex-grow rounded-md border-gray-300 shadow-sm';
-    sentenceDiv.appendChild(input);
-
-    const result = document.createElement('p');
-    result.className = 'result mt-2 ml-2 text-gray-500 text-sm';
-    sentenceDiv.appendChild(result);
-
-    sentences.appendChild(sentenceDiv);
-});
-
-document.getElementById('add').addEventListener('click', function() {
-    const sentences = document.getElementById('sentences');
-    const sentenceDiv = document.createElement('div');
-    sentenceDiv.className = 'sentence flex items-center ';
-
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'X';
-    removeButton.className = 'remove text-xs px-1 py-0.5 bg-red-500 text-white rounded';
-    removeButton.addEventListener('click', function() {
-        sentences.removeChild(sentenceDiv);
-    });
-    sentenceDiv.appendChild(removeButton);
-
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.className = 'mt-2 ml-2 flex-grow rounded-md border-gray-300 shadow-sm';
-    sentenceDiv.appendChild(input);
-
-    const result = document.createElement('p');
-    result.className = 'result mt-2 ml-2 text-gray-500 text-sm';
-    sentenceDiv.appendChild(result);
-
-    sentences.appendChild(sentenceDiv);
-});
-
 document.getElementById('validateAll').addEventListener('click', function() {
     const sentences = document.querySelectorAll('.sentence');
     let score = 0;
+    let validSentences = 0;
 
-    const pluralSubjects = ['we', 'they', 'these', 'those'];
-    const singularSubjects = ['i', 'you', 'he', 'she', 'it', 'this', 'that'];
+    const singularSubjects = ["i", "he", "she", "it", "this", "that"];
+    const pluralSubjects = ["you", "we", "they", "these", "those"];
     const singularVerb = "was";
     const pluralVerb = "were";
 
@@ -65,27 +16,30 @@ document.getElementById('validateAll').addEventListener('click', function() {
         let subject = words[0];
         let verb = words[1];
 
-        // Check for defined article "the"
         if (subject === 'the') {
             subject = words[1];
             verb = words[2];
         }
 
-        if ((singularSubjects.includes(subject) && verb === singularVerb) || 
-            (pluralSubjects.includes(subject) && verb === pluralVerb) ||
-            (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] !== 's' && verb === singularVerb) ||
-            (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] === 's' && verb === pluralVerb)) {
-            result.textContent = 'La frase es válida.';
-            result.style.color = 'green';
-            score++;
-        } else {
-            result.textContent = 'La frase no es válida.';
-            result.style.color = 'red';
+        if (input.value.trim() !== '') {
+            validSentences++;
+            if ((singularSubjects.includes(subject) && verb === singularVerb) || 
+                (pluralSubjects.includes(subject) && verb === pluralVerb) ||
+                (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] !== 's' && verb === singularVerb) ||
+                (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] === 's' && verb === pluralVerb) ||
+                (subject[0] === subject[0].toUpperCase() && verb === singularVerb)) {
+                result.textContent = 'La frase es válida.';
+                result.style.color = 'green';
+                score++;
+            } else {
+                result.textContent = 'La frase no es válida.';
+                result.style.color = 'red';
+            }
         }
     });
 
     const scoreElement = document.getElementById('score');
-    const scoreValue = ((score / sentences.length) * 5).toFixed(1);
+    const scoreValue = ((score / validSentences) * 5).toFixed(1);
     scoreElement.textContent = 'Puntuación: ' + scoreValue;
 
     const motivationalMessageElement = document.getElementById('motivationalMessage');
@@ -96,47 +50,4 @@ document.getElementById('validateAll').addEventListener('click', function() {
     } else {
         motivationalMessageElement.textContent = 'No te desanimes, sigue intentándolo.';
     }
-});
-
-
-
-
-
-
-
-var openModal = document.querySelector('#openModal');
-var closeModal = document.querySelector('.modal-close');
-var modal = document.querySelector('.modal');
-
-openModal.addEventListener('click', function() {
-    modal.classList.remove('opacity-0');
-    modal.classList.remove('pointer-events-none');
-    document.body.classList.add('modal-active');
-});
-
-closeModal.addEventListener('click', function() {
-    modal.classList.add('opacity-0');
-    modal.classList.add('pointer-events-none');
-    document.body.classList.remove('modal-active');
-});
-
-
-
-
-
-
-var openModal = document.querySelector('#openModal');
-var closeModal = document.querySelector('.modal-close');
-var modal = document.querySelector('.modal');
-
-openModal.addEventListener('click', function() {
-    modal.classList.remove('opacity-0');
-    modal.classList.remove('pointer-events-none');
-    document.body.classList.add('modal-active');
-});
-
-closeModal.addEventListener('click', function() {
-    modal.classList.add('opacity-0');
-    modal.classList.add('pointer-events-none');
-    document.body.classList.remove('modal-active');
 });
