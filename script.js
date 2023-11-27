cc3e243c035a8a812d30eae86446478d90ff2521
Anderson Mendoza <74158361+andermendz@users.noreply.github.com>
2023-11-27 14:55:08 -0500

    added new regex

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
+
+document.getElementById('add').addEventListener('click', function() {
+    const sentences = document.getElementById('sentences');
+    const sentenceDiv = document.createElement('div');
+    sentenceDiv.className = 'sentence flex items-center ';
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'X';
+    removeButton.className = 'remove text-xs px-1 py-0.5 bg-red-500 text-white rounded';
+    removeButton.addEventListener('click', function() {
+        sentences.removeChild(sentenceDiv);
+    });
+    sentenceDiv.appendChild(removeButton);
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'mt-2 ml-2 flex-grow rounded-md border-gray-300 shadow-sm';
+    sentenceDiv.appendChild(input);
+
+    const result = document.createElement('p');
+    result.className = 'result mt-2 ml-2 text-gray-500 text-sm';
+    sentenceDiv.appendChild(result);
+
+    sentences.appendChild(sentenceDiv);
+});
+
+
 document.getElementById('validateAll').addEventListener('click', function() {
     const sentences = document.querySelectorAll('.sentence');
     let score = 0;
     let validSentences = 0;
 
-    const singularSubjects = ["i", "he", "she", "it", "this", "that"];
-    const pluralSubjects = ["you", "we", "they", "these", "those"];
-    const singularVerb = "was";
-    const pluralVerb = "were";
+    const singularPattern = /^(i|he|she|it|this|that|the\s[a-z]+[^s])\swas/i;
+    const pluralPattern = /^(you|we|they|these|those|the\s[a-z]+s)\swere/i;
 
     sentences.forEach(sentence => {
         const input = sentence.querySelector('input');
         const result = sentence.querySelector('.result');
 
-        const words = input.value.toLowerCase().split(' ');
-        let subject = words[0];
-        let verb = words[1];
-
-        if (subject === 'the') {
-            subject = words[1];
-            verb = words[2];
-        }
-
         if (input.value.trim() !== '') {
             validSentences++;
-            if ((singularSubjects.includes(subject) && verb === singularVerb) || 
-                (pluralSubjects.includes(subject) && verb === pluralVerb) ||
-                (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] !== 's' && verb === singularVerb) ||
-                (!singularSubjects.includes(subject) && !pluralSubjects.includes(subject) && subject[subject.length-1] === 's' && verb === pluralVerb) ||
-                (subject[0] === subject[0].toUpperCase() && verb === singularVerb)) {
+            if (singularPattern.test(input.value) || pluralPattern.test(input.value)) {
                 result.textContent = 'La frase es válida.';
                 result.style.color = 'green';
                 score++;
@@ -50,4 +62,23 @@ document.getElementById('validateAll').addEventListener('click', function() {
     } else {
         motivationalMessageElement.textContent = 'No te desanimes, sigue intentándolo.';
     }
+});
+
+
+
+
+var openModal = document.querySelector('#openModal');
+var closeModal = document.querySelector('.modal-close');
+var modal = document.querySelector('.modal');
+
+openModal.addEventListener('click', function() {
+    modal.classList.remove('opacity-0');
+    modal.classList.remove('pointer-events-none');
+    document.body.classList.add('modal-active');
+});
+
+closeModal.addEventListener('click', function() {
+    modal.classList.add('opacity-0');
+    modal.classList.add('pointer-events-none');
+    document.body.classList.remove('modal-active');
 });
