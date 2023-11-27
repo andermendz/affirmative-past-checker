32615432e4d92b004c30ba20593ceb658f28df1d
andermendz <andersonmendz19@gmail.com>
2023-11-26 19:10:37 -0500

    added score

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 document.getElementById('add').addEventListener('click', function() {
     const sentences = document.getElementById('sentences');
     const sentenceDiv = document.createElement('div');
-    sentenceDiv.className = 'sentence';
+    sentenceDiv.className = 'sentence flex items-center';
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'X';
+    removeButton.className = 'remove px-1 py-0.5 bg-red-500 text-white rounded';
+    removeButton.addEventListener('click', function() {
+        sentences.removeChild(sentenceDiv);
+    });
+    sentenceDiv.appendChild(removeButton);
 
     const input = document.createElement('input');
     input.type = 'text';
-    input.className = 'mt-2 block w-full rounded-md border-gray-300 shadow-sm';
+    input.className = 'mt-2 ml-2 flex-grow rounded-md border-gray-300 shadow-sm';
     sentenceDiv.appendChild(input);
 
     const result = document.createElement('p');
-    result.className = 'result mt-2 text-gray-500 text-sm';
+    result.className = 'result mt-2 ml-2 text-gray-500 text-sm';
     sentenceDiv.appendChild(result);
 
     sentences.appendChild(sentenceDiv);
 });
 
+
 document.getElementById('validateAll').addEventListener('click', function() {
     const sentences = document.getElementsByClassName('sentence');
+    let score = 0;
 
     for (let i = 0; i < sentences.length; i++) {
         const sentence = sentences[i].getElementsByTagName('input')[0].value;
@@ -38,6 +48,20 @@ document.getElementById('validateAll').addEventListener('click', function() {
         } else {
             result.textContent = 'La frase es válida.';
             result.style.color = 'green';
+            score++;
         }
+    }
+
+    const scoreElement = document.getElementById('score');
+    const scoreValue = ((score / sentences.length) * 5).toFixed(1);
+    scoreElement.textContent = 'Puntuación: ' + scoreValue;
+
+    const motivationalMessageElement = document.getElementById('motivationalMessage');
+    if (scoreValue >= 4) {
+        motivationalMessageElement.textContent = '¡Excelente trabajo! Sigue así.';
+    } else if (scoreValue >= 3) {
+        motivationalMessageElement.textContent = 'Buen trabajo, pero puedes hacerlo mejor.';
+    } else {
+        motivationalMessageElement.textContent = 'No te desanimes, sigue intentándolo.';
     }
 });
