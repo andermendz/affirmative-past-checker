ea9a9b974f9eb805c8bd30e65b7ce0b8e3db82d3
andermendz <andersonmendz19@gmail.com>
2023-11-26 19:44:49 -0500

    added singular and plural validation

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('add').addEventListener('click', function() {
     const sentences = document.getElementById('sentences');
     const sentenceDiv = document.createElement('div');
-    sentenceDiv.className = 'sentence flex items-center';
+    sentenceDiv.className = 'sentence flex items-center ';
 
     const removeButton = document.createElement('button');
     removeButton.textContent = 'X';
-    removeButton.className = 'remove px-1 py-0.5 bg-red-500 text-white rounded';
+    removeButton.className = 'remove text-xs px-1 py-0.5 bg-red-500 text-white rounded';
     removeButton.addEventListener('click', function() {
         sentences.removeChild(sentenceDiv);
     });
@@ -33,14 +33,21 @@ document.getElementById('validateAll').addEventListener('click', function() {
         const result = sentences[i].getElementsByClassName('result')[0];
 
         const subjectRegex = /^(The\s[a-z]+|[A-Z][a-z]*|I|You|He|She|It|We|(This|That|These|Those)\s[a-z]+)\s/i;
-        const verbRegex = /\s(am|are|is|was|were)\s/i;
+        const verbRegexSingular = /\s(am|is|was)\s/i;
+        const verbRegexPlural = /\s(are|were)\s/i;
         const complementRegex = /\s(am|are|is|was|were)\s[a-z\s]*$/i;
 
         if (!subjectRegex.test(sentence)) {
             result.textContent = 'Falta el sujeto en la frase.';
             result.style.color = 'red';
-        } else if (!verbRegex.test(sentence)) {
+        } else if (subjectRegex.test(sentence) && !verbRegexSingular.test(sentence) && !verbRegexPlural.test(sentence)) {
             result.textContent = 'Falta el verbo en la frase.';
+            result.style.color = 'red';
+        } else if (subjectRegex.test(sentence) && verbRegexSingular.test(sentence) && sentence.match(subjectRegex)[0].endsWith('s ')) {
+            result.textContent = 'El sujeto y el verbo no concuerdan. El sujeto es plural pero el verbo es singular.';
+            result.style.color = 'red';
+        } else if (subjectRegex.test(sentence) && verbRegexPlural.test(sentence) && !sentence.match(subjectRegex)[0].endsWith('s ')) {
+            result.textContent = 'El sujeto y el verbo no concuerdan. El sujeto es singular pero el verbo es plural.';
             result.style.color = 'red';
         } else if (!complementRegex.test(sentence)) {
             result.textContent = 'Falta el complemento en la frase.';
@@ -51,7 +58,6 @@ document.getElementById('validateAll').addEventListener('click', function() {
             score++;
         }
     }
-
     const scoreElement = document.getElementById('score');
     const scoreValue = ((score / sentences.length) * 5).toFixed(1);
     scoreElement.textContent = 'Puntuación: ' + scoreValue;
